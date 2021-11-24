5d4207233d1371d3511531250de36208e8fc0cff
Lukasz Macon <luk.macon@gmail.com>
2021-11-24 14:22:06 +0100

    Updates

---
+++ precache-manifest.b776f6ea2c8e1400863c18709edf0f33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64fe2cbd63dd221410948a197ded6269",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b394d921c02840840bcf",
+    "url": "/static/js/2.ab803e51.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ab803e51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f51c0320d030a0fe6e0c",
+    "url": "/static/js/main.db2d32d7.chunk.js"
+  },
+  {
+    "revision": "d16bdf43e7a3709c18e4",
+    "url": "/static/js/runtime-main.4f5e367e.js"
+  }
+]);
