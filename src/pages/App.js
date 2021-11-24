34e8f846d1586b95e38e00fe98cc5b7f3c7a20fa
Lukasz Macon <luk.macon@gmail.com>
2021-11-24 14:51:32 +0100

    add media device func

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,30 @@
-import React, { memo } from "react";
+import React, { memo, useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    function headphone() {
+      // return navigator.mediaDevices.enumerateDevices().then(devices => {
+      //   const audioDevices = devices.filter(device => device.kind == 'audiooutput')
+      //   const activeDevice = audioDevices[0]
+      //   return activeDevice.label.indexOf('(Bluetooth)') !== -1
+      // })
+
+      alert("changed");
+      // Maybe use enumerateDevices() to see what connected
+    }
+
+    console.log(navigator.mediaDevices);
+
+    alert(navigator.mediaDevices);
+
+    navigator.mediaDevices.addEventListener("devicechange", () => headphone());
+    return () => {
+      navigator.mediaDevices.removeEventListener("devicechange", () =>
+        headphone()
+      );
+    };
+  }, []);
+
   return <div>Hello gh-cd test</div>;
 };
 
